1f1a8f39cb76bc7de69be933a14bfe609c01a46f
Andrea Barghigiani <a.barghigiani@gmail.com>
2023-05-16 04:25:45 +0200

    feat: add redirect after Project creation

--- src/pages/project/add.tsx
+++ src/pages/project/add.tsx
@@ -4,6 +4,7 @@ import { api } from "@/utils/api";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { uppercaseFirst } from "@/utils/string";
 import { Frequency } from "@prisma/client";
+import { useRouter } from "next/router";
 
 // Types
 import type { NextPage } from "next";
@@ -26,6 +27,7 @@ import {
 } from "@/components/ui/select";
 
 const AddProject: NextPage = () => {
+  const router = useRouter();
   const { register, handleSubmit, control } = useForm<ProjectValues>({
     resolver: zodResolver(ProjectValuesSchema),
     defaultValues: {
@@ -34,7 +36,11 @@ const AddProject: NextPage = () => {
     },
   });
 
-  const { mutate } = api.project.create.useMutation();
+  const { mutate } = api.project.create.useMutation({
+    onSuccess: async () => {
+      await router.push(`/projects/`);
+    },
+  });
 
   const onSubmit: SubmitHandler<ProjectValues> = (data) => {
     data.increaseFrequency = Frequency[data.increaseFrequency] as Frequency;
@@ -42,100 +48,94 @@ const AddProject: NextPage = () => {
   };
 
   return (
-    <>
-      <main className="container mx-auto">
-        <Heading size="page" gradient="gold" spacing="massive">
-          Add Project
-        </Heading>
+    <main className="container mx-auto">
+      <Heading size="page" gradient="gold" spacing="massive">
+        Add Project
+      </Heading>
 
-        <form className="space-y-3" onSubmit={handleSubmit(onSubmit)}>
+      <form className="space-y-3" onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <Label htmlFor="name">Project Name</Label>
+          <Input
+            type="text"
+            placeholder="My new fantastic project"
+            id="name"
+            {...register("name")}
+          />
+        </div>
+
+        <div>
+          <Label htmlFor="description">Project description</Label>
+          <Textarea
+            id="description"
+            placeholder="Add a description if you wish"
+            {...register("description")}
+          />
+        </div>
+
+        <section className="flex items-end gap-5">
           <div>
-            <Label htmlFor="name">Project Name</Label>
+            <Label htmlFor="initial">Initial Deposit</Label>
             <Input
-              type="text"
-              placeholder="My new fantastic project"
-              id="name"
-              {...register("name")}
+              type="number"
+              id="initial"
+              step=".01"
+              {...register("initial", { valueAsNumber: true })}
             />
           </div>
 
           <div>
-            <Label htmlFor="description">Project description</Label>
-            <Textarea
-              id="description"
-              placeholder="Add a description if you wish"
-              {...register("description")}
+            <Label htmlFor="increaseFrequency">Project frequency</Label>
+            <Controller
+              control={control}
+              name="increaseFrequency"
+              render={({ field }) => (
+                <Select onValueChange={field.onChange}>
+                  <SelectTrigger className="w-36">
+                    <SelectValue placeholder="Frequency..." />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {Object.values(Frequency).map((f: Frequency) => (
+                      <SelectItem className="cursor-pointer" key={f} value={f}>
+                        {uppercaseFirst(f)}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              )}
             />
           </div>
 
-          <section className="flex items-end gap-5">
-            <div>
-              <Label htmlFor="initial">Initial Deposit</Label>
-              <Input
-                type="number"
-                id="initial"
-                step=".01"
-                {...register("initial", { valueAsNumber: true })}
-              />
-            </div>
+          <div>
+            <Label htmlFor="increaseAmount">Increase Amount</Label>
+            <Input
+              type="number"
+              id="increaseAmount"
+              step=".01"
+              {...register("increaseAmount", { valueAsNumber: true })}
+            />
+          </div>
 
-            <div>
-              <Label htmlFor="increaseFrequency">Project frequency</Label>
-              <Controller
-                control={control}
-                name="increaseFrequency"
-                render={({ field }) => (
-                  <Select onValueChange={field.onChange}>
-                    <SelectTrigger className="w-36">
-                      <SelectValue placeholder="Frequency..." />
-                    </SelectTrigger>
-                    <SelectContent>
-                      {Object.values(Frequency).map((f: Frequency) => (
-                        <SelectItem
-                          className="cursor-pointer"
-                          key={f}
-                          value={f}
-                        >
-                          {uppercaseFirst(f)}
-                        </SelectItem>
-                      ))}
-                    </SelectContent>
-                  </Select>
-                )}
-              />
-            </div>
+          <div className="flex items-center">
+            <Label htmlFor="compound">Does it compound?</Label>
+            <Controller
+              control={control}
+              name="compound"
+              render={({ field }) => (
+                <Switch
+                  onCheckedChange={field.onChange}
+                  checked={field.value}
+                  ref={field.ref}
+                  className="ml-2"
+                />
+              )}
+            />
+          </div>
+        </section>
 
-            <div>
-              <Label htmlFor="increaseAmount">Increase Amount</Label>
-              <Input
-                type="number"
-                id="increaseAmount"
-                step=".01"
-                {...register("increaseAmount", { valueAsNumber: true })}
-              />
-            </div>
-
-            <div className="flex items-center">
-              <Label htmlFor="compound">Does it compound?</Label>
-              <Controller
-                control={control}
-                name="compound"
-                render={({ field }) => (
-                  <Switch
-                    onCheckedChange={field.onChange}
-                    checked={field.value}
-                    ref={field.ref}
-                    className="ml-2"
-                  />
-                )}
-              />
-            </div>
-          </section>
-
-          <Button type="submit">Save Project</Button>
-        </form>
-      </main>
-    </>
+        <Button type="submit">Save Project</Button>
+      </form>
+    </main>
   );
 };
 
