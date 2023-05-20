9021ffea5bfd41ad9110e87829f9d5c7e81dd734
Andrea Barghigiani <a.barghigiani@gmail.com>
2023-05-20 07:20:27 +0200

    feat: added CDN imaged from Coinranking

--- next.config.mjs
+++ next.config.mjs
@@ -18,5 +18,13 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.coinranking.com",
+      },
+    ],
+  },
 };
 export default config;
