c18b1042536353856124a35fe737110d505d5f11
Andrea Barghigiani <a.barghigiani@gmail.com>
2023-05-20 07:20:58 +0200

    feat: added Next.js API to create and delete tokens list

---
+++ src/pages/api/cron/tokens/deleteAll.ts
@@ -0,0 +1,25 @@
+import { TRPCError } from "@trpc/server";
+import { getHTTPStatusCodeFromError } from "@trpc/server/http";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const prisma = new PrismaClient();
+
+  try {
+    await prisma.token.deleteMany();
+    res.status(200).json({ message: "ok" });
+  } catch (cause) {
+    if (cause instanceof TRPCError) {
+      const httpStatusCode = getHTTPStatusCodeFromError(cause);
+      res.status(httpStatusCode).json({ message: cause.message });
+      return;
+    }
+    res.status(500).json({
+      error: { message: `Something has gone wrong` },
+    });
+  }
+}
