1a8b7de1519364c07c9dd3a4fe8c78f148dfc85f
Andrea Barghigiani <a.barghigiani@gmail.com>
2023-06-09 05:20:36 +0200

    feat: added UI to delete user profile

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,5 +1,6 @@
 // Utils
 import { api } from "@/utils/api";
+import { useRouter } from "next/router";
 
 // Types
 import type { NextPage } from "next";
@@ -7,10 +8,22 @@ import type { NextPage } from "next";
 // Components
 import Heading from "@/components/ui/heading";
 import ProfileForm from "@/components/profileForm";
+import { Button } from "@/components/ui/button";
 
 const Profile: NextPage = () => {
+  const router = useRouter();
+
   const { data: wallet, isSuccess: isWalletSuccess } =
     api.wallet.get.useQuery();
+  const { mutate } = api.user.delete.useMutation({
+    onSuccess: async () => {
+      await router.push(`/`);
+    },
+  });
+
+  const handleDelete = () => {
+    mutate();
+  };
 
   if (!wallet) return <div>Something wrong.</div>;
 
@@ -19,6 +32,10 @@ const Profile: NextPage = () => {
       <Heading size="page" gradient="gold" spacing="massive">
         Profile
       </Heading>
+
+      <Button variant="destructive" className="mx-auto" onClick={handleDelete}>
+        Delete
+      </Button>
 
       <p className="text-center">
         Here you can customize aspects of your profile, for now we let you
