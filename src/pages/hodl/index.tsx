568cb6143e2621284d73b5beeb4ba6ce53a0425e
Andrea Barghigiani <a.barghigiani@gmail.com>
2023-06-09 06:31:54 +0200

    feat: added loading state to Holds index

--- src/pages/hodl/index.tsx
+++ src/pages/hodl/index.tsx
@@ -9,10 +9,14 @@ import Heading from "@/components/ui/heading";
 import HodlCard from "@/components/ui/custom/HodlCard";
 import { buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const Hodl: NextPage = () => {
-  const { data: positions, isSuccess: isPositionsSuccess } =
-    api.hodl.getByCurrentUser.useQuery();
+  const {
+    data: positions,
+    isSuccess: isPositionsSuccess,
+    isLoading: isPositionLoading,
+  } = api.hodl.getByCurrentUser.useQuery();
 
   return (
     <div>
@@ -20,32 +24,40 @@ const Hodl: NextPage = () => {
         Your positions
       </Heading>
 
-      {!!positions && (
-        <div className="my-4 space-y-3 text-center text-stone-400">
-          <Link
-            className={buttonVariants({ className: "ml-auto" })}
-            href="/hodl/add"
-          >
-            New position
-          </Link>
-          <p className=" text-lg ">
-            Here you go, track and compare your positions.
-          </p>
-        </div>
-      )}
-
-      {!positions && (
-        <div className="space-y-3 text-center text-stone-400">
-          <Heading>Looks like you have no positions open, yet!</Heading>
-          <p>Just click the button below and start track your investments.</p>
-          <Link
-            className={buttonVariants({ className: "ml-auto" })}
-            href="/hodl/add"
-          >
-            Create one
-          </Link>
-        </div>
-      )}
+      <div className="my-4 space-y-3 text-center text-stone-400">
+        {isPositionLoading ? (
+          <>
+            <Skeleton as="h1" className="mx-auto h-10 w-64 bg-dog-400" />
+            <Skeleton as="p" className="mx-auto h-6 w-144 bg-dog-400 " />
+            <Skeleton as="a" className="mx-auto block h-8 w-32 bg-dog-400" />
+          </>
+        ) : !!positions ? (
+          <>
+            <Heading>Congrats for tracking your positions!</Heading>
+            <p className=" text-lg ">
+              Here you can have a feeling on how your investments are going, or
+              create a new one.
+            </p>
+            <Link
+              className={buttonVariants({ className: "ml-auto" })}
+              href="/hodl/add"
+            >
+              New position
+            </Link>
+          </>
+        ) : (
+          <>
+            <Heading>Looks like you have no positions open, yet!</Heading>
+            <p>Just click the button below and start track your investments.</p>
+            <Link
+              className={buttonVariants({ className: "ml-auto" })}
+              href="/hodl/add"
+            >
+              Create one
+            </Link>
+          </>
+        )}
+      </div>
 
       {isPositionsSuccess && (
         <div className="grid grid-cols-2 gap-4">
