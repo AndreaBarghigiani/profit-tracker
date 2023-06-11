011a8ae2ce1674a4cbdc96cafd061df6ff5c74cb
Andrea Barghigiani <a.barghigiani@gmail.com>
2023-06-11 08:31:26 +0200

    feat: unified creation or addition to Hodl positions

---
+++ src/pages/hodl/create/[coingecko_id].tsx
@@ -0,0 +1,55 @@
+// Utils
+import { prisma } from "@/server/db";
+import { getToken } from "@/server/api/routers/token";
+
+// Types
+import type {
+  InferGetServerSidePropsType,
+  GetServerSidePropsContext,
+  NextPage,
+} from "next";
+import type { TokenWithoutDates } from "@/server/types";
+
+// Components
+import AddHodlPositionForm from "@/components/addHodlPositionForm";
+import Heading from "@/components/ui/heading";
+
+const AddHodlPosition: NextPage<
+  InferGetServerSidePropsType<typeof getServerSideProps>
+> = ({ token, hodlId }) => {
+  return (
+    <div>
+      <Heading size="page" gradient="gold" spacing="massive">
+        Your positions for {token.name}
+      </Heading>
+      <p className="mb-4 text-center text-lg text-stone-400">
+        Are you buying or selling?
+      </p>
+
+      <AddHodlPositionForm hodlId={hodlId} token={token} />
+    </div>
+  );
+};
+
+export default AddHodlPosition;
+
+export async function getServerSideProps(
+  context: GetServerSidePropsContext<{ coingecko_id: string }>
+) {
+  if (!context.params?.coingecko_id) return;
+
+  const token: TokenWithoutDates = await getToken({
+    tokenId: context.params?.coingecko_id,
+    prisma,
+  });
+
+  delete token.createdAt;
+  delete token.updatedAt;
+
+  return {
+    props: {
+      token: token,
+      hodlId: null,
+    },
+  };
+}
