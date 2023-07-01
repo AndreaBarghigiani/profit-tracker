bf29108e7ea732ff4a61601a2dfb347c77bfee6f
Andrea Barghigiani <a.barghigiani@gmail.com>
2023-07-01 07:02:12 +0200

    feat: improved Project logic and displayed info

--- src/server/api/routers/transaction/addInterest.ts
+++ src/server/api/routers/transaction/addInterest.ts
@@ -12,7 +12,11 @@ const mapFrequency: Record<string, number> = {
   [Frequency.YEARLY]: 365 * 23 * 60 * 60 * 1000, // 365 days in milliseconds
 };
 
-export const addInterest = async (projectId: string, prisma: PrismaClient) => {
+export const addInterest = async (
+  projectId: string,
+  prisma: PrismaClient,
+  skip: boolean
+) => {
   const project = await getProject({ projectId, prisma });
 
   const projectFrequencyHours = mapFrequency[
@@ -28,44 +32,35 @@ export const addInterest = async (projectId: string, prisma: PrismaClient) => {
 
   const timeDiff = currentTime - lastTransaction.createdAt.getTime() - 1;
 
-  if (timeDiff < projectFrequencyHours) return;
+  if (timeDiff < projectFrequencyHours && !skip) return;
 
   // 1. calculate interest amount
   const amount: number = project.compound
-    ? project.currentHolding * (project.increaseAmount / 100)
-    : project.initial * (project.increaseAmount / 100);
+    ? project.moneyAtWork * (project.increaseAmount / 100)
+    : project.deposit * (project.increaseAmount / 100);
 
-  // 2. add new transaction with interest amount
-  await prisma.transaction.create({
-    data: {
-      amount,
-      evaluation: amount,
-      projectId: project.id,
-      type: TransactionType.INTEREST,
-    },
-  });
-
-  // 3. update project currentHolding
+  // 2. update project currentHolding
   await prisma.project.update({
     where: {
       id: project.id,
     },
     data: {
-      currentHolding: {
+      interest: {
         increment: amount,
       },
+      // Indreases only if compounding
+      ...(project.compound && {
+        moneyAtWork: {
+          increment: amount,
+        },
+      }),
       accruing: true,
-    },
-  });
-
-  // 4. update wallet with new project holdings
-  await prisma.wallet.update({
-    where: {
-      userId: project.userId,
-    },
-    data: {
-      total: {
-        increment: amount,
+      transaction: {
+        create: {
+          amount,
+          evaluation: amount,
+          type: TransactionType.INTEREST,
+        },
       },
     },
   });
