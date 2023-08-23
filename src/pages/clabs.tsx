ea2ecf89a8a5415d616fa7a34a33753b0b7c497a
Andrea Barghigiani <a.barghigiani@gmail.com>
2023-08-23 16:04:10 +0200

    feat: added new /clabs page

---
+++ src/pages/clabs.tsx
@@ -0,0 +1,46 @@
+// Utils
+import { getServerAuthSession } from "@/server/auth";
+
+// Types
+import type { NextPageWithLayout } from "./_app";
+import type { IncomingMessage, ServerResponse } from "http";
+
+// Components
+import LayoutMarketing from "@/components/layoutMarketing";
+import HeroSection from "@/components/ui/custom/CLabs/HeroSectionCLabs";
+
+const Home: NextPageWithLayout = () => {
+  return (
+    <>
+      <HeroSection />
+    </>
+  );
+};
+
+Home.getLayout = function getLayout(page: React.ReactElement) {
+  return <LayoutMarketing>{page}</LayoutMarketing>;
+};
+
+export default Home;
+
+export async function getServerSideProps(context: {
+  req: IncomingMessage & {
+    cookies: Partial<{ [key: string]: string }>;
+  };
+  res: ServerResponse<IncomingMessage>;
+}) {
+  const session = await getServerAuthSession(context);
+
+  if (session) {
+    return {
+      redirect: {
+        destination: "/dashboard",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
