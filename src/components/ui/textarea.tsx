b0321ecd02f23e5fdf9fc08f5d4c160d45d5d7ac
Andrea Barghigiani <a.barghigiani@gmail.com>
2023-08-26 18:35:38 +0200

    feat: minor improv Input and Textarea components

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -9,14 +9,14 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          "flex h-20 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-          className
+          "flex h-20 w-full rounded-md border border-input bg-dog-900 px-3 py-2 text-sm placeholder:text-dog-700 focus:z-10 focus:outline-none focus:ring-2 focus:ring-main-600 focus:ring-offset-main-700 disabled:cursor-not-allowed disabled:opacity-50",
+          className,
         )}
         ref={ref}
         {...props}
       />
     );
-  }
+  },
 );
 Textarea.displayName = "Textarea";
 
