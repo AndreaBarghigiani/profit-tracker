a6edb94d83107d52e4c7de28c436f28da6cda292
Andrea Barghigiani <a.barghigiani@gmail.com>
2023-09-04 19:21:52 +0200

    feat: new UserStats Cards

--- src/components/ui/custom/UserStats.tsx
+++ src/components/ui/custom/UserStats.tsx
@@ -1,21 +1,17 @@
 // Utils
 import { currencyConverter } from "@/utils/string";
 import clsx from "clsx";
+import { cn } from "@/lib/utils";
 
 // Types
 import type { Wallet } from "@prisma/client";
 import type { MassagedSumTxItem } from "@/server/types";
 import type { ReactElement } from "react";
+import type { LucideIcon } from "lucide-react";
 
 // Components
-import Heading from "@/components/ui/heading";
-import {
-  AlertTriangle,
-  DollarSign,
-  Percent,
-  CalendarRange,
-} from "lucide-react";
-import { Separator } from "@/components/ui/separator";
+import { DollarSign, DoorOpen, Banknote } from "lucide-react";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 
 type UserStatsCardProps = {
   userStats: {
@@ -32,88 +28,76 @@ type UserStatsCardProps = {
 
 const UserStats = ({ userStats, orientation }: UserStatsCardProps) => {
   const wrapperClasses = clsx(
-    "flex justify-around rounded-lg border border-dog-800 bg-dog-900 px-5 py-3 shadow-lg",
+    "md:grid md:grid-cols-4 md:gap-4 rounded-lg space-y-4 md:space-y-0",
     {
-      "h-28 items-center": orientation !== "vertical",
       "flex-col h-128 w-56": orientation === "vertical",
     },
   );
 
-  const separatorOrientation =
-    orientation === "vertical" ? "horizontal" : "vertical";
-
   return (
     <div className={wrapperClasses}>
-      <UserStatsCol
-        title="Exposure"
-        value={userStats.totals.exposure}
-        rightIcon={<AlertTriangle className="mr-2 h-4 w-4 flex-shrink-0" />}
-      />
-      <Separator
-        orientation={separatorOrientation}
-        decorative
-        className="bg-dog-600"
-      />
+      <UserCard title="Liquid Funds" Icon={DollarSign}>
+        <div className="text-3xl font-bold">
+          {currencyConverter({ amount: userStats.wallet.liquidFunds })}
+        </div>
+      </UserCard>
 
-      <UserStatsCol
-        title="Profits"
-        value={userStats.totals.profits}
-        rightIcon={<DollarSign className="mr-2 h-4 w-4 flex-shrink-0" />}
-      />
-      <Separator
-        orientation={separatorOrientation}
-        decorative
-        className="bg-dog-600"
-      />
+      <UserCard title="Exposed" Icon={DoorOpen}>
+        <div className="text-3xl font-bold">
+          {currencyConverter({ amount: userStats.totals.exposure })}
+        </div>
+      </UserCard>
 
-      <UserStatsCol
-        title="Interests"
-        value={userStats.interests.amount}
-        rightIcon={<Percent className="mr-2 h-4 w-4 flex-shrink-0" />}
-      />
-      <Separator
-        orientation={separatorOrientation}
-        decorative
-        className="bg-dog-600"
-      />
+      <UserCard title="Profits" Icon={Banknote}>
+        <div className="text-3xl font-bold">
+          {currencyConverter({ amount: userStats.totals.profits })}
+        </div>
+      </UserCard>
 
-      <UserStatsCol
-        title="Daily Target"
-        value={userStats.wallet.dailyProfit}
-        rightIcon={<CalendarRange className="mr-2 h-4 w-4 flex-shrink-0" />}
-      />
-    </div>
-  );
-};
-
-const UserStatsCol = ({
-  title,
-  value,
-  leftIcon,
-  rightIcon,
-}: {
-  title: string;
-  value: number;
-  leftIcon?: ReactElement;
-  rightIcon?: ReactElement;
-}) => {
-  return (
-    <div>
-      <header>
-        <Heading size="h3" className="mt-0 flex items-center text-dog-500">
-          {!!rightIcon && rightIcon}
-          {title}
-          {!!leftIcon && leftIcon}
-        </Heading>
-      </header>
-
-      <section>
-        <p className="text-3xl font-semibold">
-          {currencyConverter({ amount: value })}
+      <UserCard title="Your next idea...">
+        <p className="text-center text-sm">
+          What would you improve?
+          <br /> Send us a feedback.
         </p>
-      </section>
+      </UserCard>
     </div>
   );
 };
 
 export default UserStats;
+
+const UserCard = ({
+  title,
+  Icon,
+  children,
+}: {
+  title: string;
+  Icon?: LucideIcon;
+  children: ReactElement;
+}) => {
+  const cardClasses = cn({
+    "border-2 border-dashed": !Icon,
+  });
+  const cardHeaderClasses = cn("flex flex-row items-stretch space-y-0 ", {
+    "justify-between pb-2": !!Icon,
+    "justify-center p-5 pb-1": !Icon,
+  });
+  const cardTitleClasses = cn("text-xl", {
+    "text-dog-300": !!Icon,
+    "text-dog-500": !Icon,
+  });
+  const cardContentClasses = cn({
+    "text-dog-500": !!Icon,
+    "text-dog-600": !Icon,
+  });
+
+  return (
+    <Card className={cardClasses}>
+      <CardHeader className={cardHeaderClasses}>
+        <CardTitle className={cardTitleClasses}>{title}</CardTitle>
+        {!!Icon && <Icon className="z-20 h-6 w-6 text-main-700" />}
+      </CardHeader>
+      <CardContent className={cardContentClasses}>{children}</CardContent>
+    </Card>
+  );
+};
