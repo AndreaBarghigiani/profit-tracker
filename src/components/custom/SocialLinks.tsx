96cb953c9946dfc0e113ebd0efae8d91e240c0d6
Andrea Barghigiani <a.barghigiani@gmail.com>
2023-09-11 06:33:59 +0200

    feat: introduced social links

---
+++ src/components/custom/SocialLinks.tsx
@@ -0,0 +1,37 @@
+// Utils
+
+// Types
+import type { LucideIcon } from "lucide-react";
+
+// Components
+import { Twitter, Youtube } from "lucide-react";
+type SocialLinkProps = {
+  url: string;
+  Icon: LucideIcon;
+  name: string;
+};
+
+const socialLinks: SocialLinkProps[] = [
+  {
+    url: "https://twitter.com/udogtracker",
+    Icon: Twitter,
+    name: "Twitter",
+  },
+  {
+    url: "https://www.youtube.com/@UnderdogTracker",
+    Icon: Youtube,
+    name: "Youtube",
+  },
+];
+
+const SocialLinks = () => {
+  return socialLinks.map(({ url, Icon, name }) => (
+    <div key={url} className="social-links">
+      <a href={url} title={name} target="_blank">
+        <Icon className="h-6 w-6 fill-inherit" />
+      </a>
+    </div>
+  ));
+};
+
+export default SocialLinks;
