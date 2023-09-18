57e18534b7d208248aed48ee28d8b7c10827fade
Andrea Barghigiani <a.barghigiani@gmail.com>
2023-09-18 05:50:54 +0200

    feat: added credits to Storyset

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,8 +3,17 @@
 
 const Footer = () => {
   return (
-    <footer className="container mx-auto mb-6 mt-10 flex max-w-4xl items-center justify-between">
-      <p className="text-sm text-dog-400">© 2023 Underdog Tracker.</p>
+    <footer className="container mx-auto mb-6 mt-10 flex max-w-4xl items-end justify-between">
+      <p className="text-sm text-dog-400">
+        © 2023 Underdog Tracker.
+        <br />
+        <span className="text-xs text-dog-600">
+          Illustrations by{" "}
+          <a target="_blank" href="https://storyset.com/people">
+            Storyset
+          </a>
+        </span>
+      </p>
 
       <p className="text-sm text-dog-400">
         <a href="mailto:andrea@cupofcraft.dev">Contact</a>
