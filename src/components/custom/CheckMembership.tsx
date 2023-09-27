f15c534adf3ff5849d23f522404f90d665f2d6d7
Andrea Barghigiani <a.barghigiani@gmail.com>
2023-09-27 06:14:26 +0200

    feat: implemented membership

--- src/components/custom/CheckMembership.tsx
+++ src/components/custom/CheckMembership.tsx
@@ -1,20 +1,32 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+
 // Utils
+import { api } from "@/utils/api";
 import { useAccount, useContractRead } from "wagmi";
 import { PublicLockV13 } from "@unlock-protocol/contracts";
 import { networks } from "@unlock-protocol/networks";
 import { Paywall } from "@unlock-protocol/paywall";
+import { fromTimestampToDate } from "@/utils/string";
+import { Role } from "@prisma/client";
 
 // Components
 import Web3SignIn from "./Web3SignIn";
 import { Button } from "@/components/ui/button";
+import Heading from "@/components/ui/heading";
+import { Card, CardHeader, CardContent } from "@/components/ui/card";
 
 // const lockAddress = "0x4025fb5018062bf3430c01ea1ff5d8b9f7fbf5a9";
 const lockAddress = "0x0633A2cEfDf8EE20D791603e7dC2889Af75f5b6B";
 
 const CheckMembership = () => {
   const paywall = new Paywall(networks);
+  const { data: user } = api.user.getUser.useQuery();
+  const { data: hasMembership } = api.user.hasMembership.useQuery();
 
   const { address, isConnected } = useAccount();
+  const { mutate: updateMembership } = api.user.updateMembership.useMutation();
 
   // Check if has membership
   const {
@@ -48,11 +60,7 @@ const CheckMembership = () => {
     functionName: "keyExpirationTimestampFor",
     enabled: !!isConnected,
     args: [tokenOwner],
-  });
-
-  console.log("hasAccess:", hasAccess);
-  console.log("tokenOwner:", tokenOwner);
-  console.log("expirationTimestamp:", expirationTimestamp);
+  }) as unknown as { data: number };
 
   const paywallConfig = {
     icon: "https://storage.unlock-protocol.com/0c09266c-7067-448c-9ca4-6d2120cbd072",
@@ -68,9 +76,13 @@ const CheckMembership = () => {
     title: "Underdog Tracker Membership",
   };
 
+  if (hasAccess && expirationTimestamp && user?.role !== Role.SUBSCRIBER) {
+    if (!hasMembership)
+      updateMembership({ expirationTime: expirationTimestamp });
+  }
+
   // Calls paywall for checkout
   const onPurchase = async () => {
-    // const provider = await connector.getProvider();
     await paywall.loadCheckoutModal(paywallConfig);
   };
 
@@ -80,22 +92,49 @@ const CheckMembership = () => {
 
   return (
     <>
-      {!isConnected && (
-        <div className="mx-auto max-w-2xl">
-          <p>You are not connected...</p>
-          <Web3SignIn paywall={paywall} />
-        </div>
-      )}
+      <div className="mx-auto max-w-2xl">
+        {!isConnected && (
+          <Card button="side">
+            <CardHeader>
+              <Heading size="h2">Connect and check your membership</Heading>
+              <p className="text-sm">
+                Looks like your main wallet is not connected. Connect it now to
+                buy or renew your membership.
+              </p>
+            </CardHeader>
+            <CardContent className="ml-auto flex flex-shrink-0 pb-0">
+              <Web3SignIn paywall={paywall} />
+            </CardContent>
+          </Card>
+        )}
 
-      {/* {isConnected && !hasAccess && ( */}
-      {isConnected && (
-        <div>
-          <div>buy one</div>
-          <Button variant="orange" onClick={onPurchase}>
-            Buy Membership
-          </Button>
-        </div>
-      )}
+        {isConnected && !hasAccess && (
+          <Card button="side">
+            <CardHeader>
+              <Heading size="h2">😱 Looks like you don&apos;t have one</Heading>
+              <p className="text-sm">
+                Buy it now and start to track your crypto automatically.
+              </p>
+            </CardHeader>
+            <CardContent className="ml-auto flex flex-shrink-0 pb-0">
+              <Button variant="orange" onClick={onPurchase}>
+                Buy Membership
+              </Button>
+            </CardContent>
+          </Card>
+        )}
+
+        {isConnected && expirationTimestamp && (
+          <Card>
+            <CardHeader>
+              <Heading size="h2">Your membership will expire the:</Heading>
+              <p className="text-sm">
+                {fromTimestampToDate(expirationTimestamp)}
+              </p>
+            </CardHeader>
+          </Card>
+        )}
+      </div>
     </>
   );
 };
--- src/server/api/routers/user/index.ts
+++ src/server/api/routers/user/index.ts
@@ -1,6 +1,7 @@
-//import { z } from "zod";
+import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
 import { redis } from "@/lib/redis";
+import { Role } from "@prisma/client";
 
 // Types
 import type { UserCache } from "@/server/types";
@@ -13,6 +14,13 @@ const getRedisUser = async (userId: string) => {
 };
 
 export const userRouter = createTRPCRouter({
+  getUser: protectedProcedure.query(({ ctx }) => {
+    return ctx.prisma.user.findUniqueOrThrow({
+      where: {
+        id: ctx.session.user.id,
+      },
+    });
+  }),
   getRole: protectedProcedure.query(({ ctx }) => {
     return ctx.prisma.user.findUniqueOrThrow({
       where: {
@@ -26,6 +34,31 @@ export const userRouter = createTRPCRouter({
   getRedisUser: protectedProcedure.query(async ({ ctx }) => {
     return (await getRedisUser(ctx.session.user.id)) as UserCache;
   }),
+  hasMembership: protectedProcedure.query(async ({ ctx }) => {
+    const user = await ctx.prisma.user.findUnique({
+      where: {
+        id: ctx.session.user.id,
+      },
+    });
+
+    if (!user) return false;
+
+    return user.role === Role.SUBSCRIBER || user.role === Role.ADMIN;
+  }),
+  updateMembership: protectedProcedure
+    .input(z.object({ expirationTime: z.number() }))
+    .mutation(async ({ ctx, input }) => {
+      const expirationTime = new Date(Number(input.expirationTime) * 1000);
+      await ctx.prisma.user.update({
+        where: {
+          id: ctx.session.user.id,
+        },
+        data: {
+          expirationDate: expirationTime,
+          role: Role.SUBSCRIBER,
+        },
+      });
+    }),
   delete: protectedProcedure.mutation(({ ctx }) => {
     return ctx.prisma.user.delete({
       where: {
