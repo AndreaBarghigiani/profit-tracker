405beaff8c50d6b64b90bdec17efb1dd360117be
Andrea Barghigiani <a.barghigiani@gmail.com>
2023-10-08 04:47:22 +0200

    reorganization of utility string funcitons

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,28 +1,17 @@
 // Types
 type DateTypes = "long" | "short" | "full" | "medium" | undefined;
 type TimeTypes = "long" | "short" | "full" | "medium" | undefined;
-
-export const uppercaseFirst = (str: string): string => {
-  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+type NotationTypes =
+  | "compact"
+  | "standard"
+  | "scientific"
+  | "engineering"
+  | undefined;
+type CurrencyConverterType = {
+  amount: number | string;
+  showSign?: boolean;
+  removeZeros?: boolean;
 };
-
-export const formatDate = (
-  date: number | Date | undefined,
-  dateType: DateTypes = "long",
-  timeType?: TimeTypes,
-) =>
-  Intl.DateTimeFormat("en", {
-    dateStyle: dateType,
-    timeStyle: !!timeType ? timeType : undefined,
-  }).format(date);
-
-export const formatTime = (
-  date: number | Date | undefined,
-  timeType: TimeTypes = "short",
-): string =>
-  Intl.DateTimeFormat("en-EN", {
-    timeStyle: timeType,
-  }).format(date);
 
 const handleDecimals = (num: string, removeZeros: boolean = false) => {
   const hasDecimal = /\./;
@@ -51,13 +40,53 @@ export const maxSignificantDigits = (
   if (num > 0) {
     return maxSignificantDigits(Math.floor(num / 10), ++count);
   }
+
   return count;
 };
 
-type CurrencyConverterType = {
-  amount: number | string;
-  showSign?: boolean;
-  removeZeros?: boolean;
+export const uppercaseFirst = (str: string): string => {
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+};
+
+export const formatDate = (
+  date: number | Date | undefined,
+  dateType: DateTypes = "long",
+  timeType?: TimeTypes,
+) =>
+  Intl.DateTimeFormat("en", {
+    dateStyle: dateType,
+    timeStyle: !!timeType ? timeType : undefined,
+  }).format(date);
+
+export const formatTime = (
+  date: number | Date | undefined,
+  timeType: TimeTypes = "short",
+): string =>
+  Intl.DateTimeFormat("en-EN", {
+    timeStyle: timeType,
+  }).format(date);
+
+export const formatNumber = (
+  number: number | string,
+  notation: NotationTypes = "compact",
+  removeZeros: boolean = false,
+) => {
+  const numeric = typeof number === "string" ? parseFloat(number) : number;
+
+  const numUnits = maxSignificantDigits(numeric);
+  const minSignificant = numUnits === 1 ? 3 : 2;
+  const maxSignifican = numUnits >= 1 ? numUnits + 2 : 2;
+
+  const formatted = new Intl.NumberFormat("en-EN", {
+    style: "decimal",
+    notation,
+    minimumSignificantDigits: notation === "standard" ? minSignificant : 2,
+    maximumSignificantDigits: notation === "standard" ? maxSignifican : 2,
+  }).format(numeric);
+
+  return notation === "standard"
+    ? handleDecimals(formatted, removeZeros)
+    : formatted;
 };
 
 export const currencyConverter = ({
@@ -79,36 +108,4 @@ export const currencyConverter = ({
   }).format(numeric);
 
   return handleDecimals(formatted, removeZeros);
-};
-
-export const formatNumber = (
-  number: number | string,
-  notation:
-    | "compact"
-    | "standard"
-    | "scientific"
-    | "engineering"
-    | undefined = "compact",
-  removeZeros: boolean = false,
-) => {
-  const numeric = typeof number === "string" ? parseFloat(number) : number;
-
-  const numUnits = maxSignificantDigits(numeric);
-  const minSignificant = numUnits === 1 ? 3 : 2;
-  const maxSignifican = numUnits >= 1 ? numUnits + 2 : 2;
-
-  const formatted = new Intl.NumberFormat("en-EN", {
-    style: "decimal",
-    notation,
-    minimumSignificantDigits: notation === "standard" ? minSignificant : 2,
-    maximumSignificantDigits: notation === "standard" ? maxSignifican : 2,
-  }).format(numeric);
-
-  // if (notation === "standard") {
-  //   return handleDecimals(formatted, removeZeros);
-  // }
-
-  return notation === "standard"
-    ? handleDecimals(formatted, removeZeros)
-    : formatted;
 };
